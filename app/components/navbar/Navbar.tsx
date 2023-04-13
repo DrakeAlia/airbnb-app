bcbf39bd209514bc5247b018649effcff2272d03
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-04-13 14:13:00 -0700

    created Container and Logo components

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,7 +1,30 @@
+import Container from "../Container";
+import Logo from "./Logo";
+
 const Navbar = () => {
   return (
-    <div>
-    I am a Navbar!
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+    py-4
+    boreder-b-[1px]
+    "
+      >
+        <Container>
+          <div
+            className="
+            flex
+            flex-row
+            items-center
+            justify-between
+            gap-3
+            md:gap-0
+            "
+          >
+            <Logo />
+          </div>
+        </Container>
+      </div>
     </div>
   );
 };
