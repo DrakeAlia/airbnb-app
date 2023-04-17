fff696d9b376211f2187a170b2621d5ea93b6655
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-04-17 16:52:14 -0700

    completed Button component, created new folder called hooks with a file called useRegisterModal.ts

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { IconType } from "react-icons";
 
 interface ButtonProps {
@@ -15,18 +17,20 @@ const Button: React.FC<ButtonProps> = ({
   disabled,
   outline,
   small,
-  icon,
+  icon: Icon,
 }) => {
   return (
     <button
+      onClick={onClick}
+      disabled={disabled}
       className={`
-    relative
-    disabled:opacity-70
-    disabled:cursor-not-allowed
-    rounded-lg
-    hover:opacity-80
-    transition-opacity
-    w-full
+        relative
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition-opacity
+        w-full
     ${outline ? "bg-white" : "bg-rose-500"}
     ${outline ? "border-black" : "border-rose-500"}
     ${outline ? "text-black" : "text-white"}
@@ -36,6 +40,15 @@ const Button: React.FC<ButtonProps> = ({
     ${small ? "border-[1px]" : "border-2"}
     `}
     >
+      {Icon && (
+        <Icon
+          size={24}
+          className="
+            absolute 
+            left-4
+            top-3"
+        />
+      )}
       {label}
     </button>
   );
--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
   actionLabel: string;
   disabled?: boolean;
   secondaryAction?: () => void;
-  secondaryLabel?: string;
+  secondaryActionLabel?: string;
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -27,7 +27,7 @@ const Modal: React.FC<ModalProps> = ({
   actionLabel,
   disabled,
   secondaryAction,
-  secondaryLabel,
+  secondaryActionLabel,
 }) => {
   const [showModal, setShowModal] = useState(isOpen);
 
@@ -156,7 +156,8 @@ const Modal: React.FC<ModalProps> = ({
               <div className="relative p-6 flex-auto">{body}</div>
               {/* FOOTER */}
               <div className="flex flex-col gap-2 p-6">
-                <div className="
+                <div
+                  className="
                 flex
                 flex-row
                 items-center
@@ -164,7 +165,19 @@ const Modal: React.FC<ModalProps> = ({
                 w-full
                 "
                 >
-                  <Button label="My Button" />
+                  {secondaryAction && secondaryActionLabel && (
+                    <Button
+                      outline
+                      disabled={disabled}
+                      label={secondaryActionLabel}
+                      onClick={handleSecondaryAction}
+                    />
+                  )}
+                  <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubmit}
+                  />
                 </div>
               </div>
             </div>
