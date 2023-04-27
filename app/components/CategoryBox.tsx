a55ae001e56fda613e64f95f347d5bd6bb867923
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-04-26 17:22:54 -0700

    added more categories with icons and updating the query for the categories in CategoryBox and Categories files

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -3,7 +3,7 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
 import { IconType } from "react-icons";
-import qs from "query-string"
+import qs from "query-string";
 
 interface CategoryBoxProps {
   icon: IconType;
@@ -23,12 +23,32 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
     let currentQuery = {};
 
     if (params) {
-        currentQuery = qs.parse(params.toString());
+      currentQuery = qs.parse(params.toString());
     }
-  }, []);
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
 
   return (
     <div
+    onClick={handleClick}
       className={`
         flex
         flex-col
--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,9 +1,22 @@
 import Container from "../Container";
 
-import { TbBeach } from "react-icons/tb";
-import { GiWindmill } from "react-icons/gi";
+import { IoDiamond } from "react-icons/io5";
+import { BsSnow } from "react-icons/bs";
+import { FaSkiing } from "react-icons/fa";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
+  GiIsland,
+  GiWindmill,
+} from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export const categories = [
   {
@@ -21,9 +34,79 @@ export const categories = [
     icon: MdOutlineVilla,
     description: "This property is modern.",
   },
+  {
+    label: "Countryside",
+    icon: TbMountain,
+    description: "This property is located in the countryside.",
+  },
+  {
+    label: "Pools",
+    icon: TbPool,
+    description: "This property has pools.",
+  },
+  {
+    label: "Islands",
+    icon: GiIsland,
+    description: "This property is located on an island.",
+  },
+  {
+    label: "Lake",
+    icon: GiBoatFishing,
+    description: "This property is located near a lake.",
+  },
+  {
+    label: "Skiing",
+    icon: FaSkiing,
+    description: "This property is located near a skiing area.",
+  },
+  {
+    label: "Castles",
+    icon: GiCastle,
+    description: "This property is located in a castle.",
+  },
+  {
+    label: "Camping",
+    icon: GiForestCamp,
+    description: "This property is located in a camping area.",
+  },
+  {
+    label: "Arctic",
+    icon: BsSnow,
+    description: "This property is located in the arctic.",
+  },
+  {
+    label: "Cave",
+    icon: GiCaveEntrance,
+    description: "This property is located in a cave.",
+  },
+  {
+    label: "Desert",
+    icon: GiCactus,
+    description: "This property is located in a desert.",
+  },
+  {
+    label: "Barns",
+    icon: GiBarn,
+    description: "This property is located in a barn.",
+  },
+  {
+    label: "Lux",
+    icon: IoDiamond,
+    description: "This property is luxurious",
+  },
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+
+  const isMainPage = pathname === "/";
+
+  if (!isMainPage) {
+    return null;
+  }
+
   return (
     <Container>
       <div
@@ -40,7 +123,7 @@ const Categories = () => {
           <CategoryBox
             key={item.label}
             label={item.label}
-            description={item.description}
+            selected={category === item.label}
             icon={item.icon}
           />
         ))}
