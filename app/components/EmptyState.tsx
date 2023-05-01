c0c830626ee2ce6d6b20af83595f0850c890fedb
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-04-30 17:13:09 -0700

    created getListings to fetch our listings and added styles to EmptyState

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,9 +1,44 @@
-"use client"
+"use client";
 
-const EmptyState = () => {
-    return (
-        <div></div>
-    )
+import { useRouter } from "next/navigation";
+import Heading from "./Heading";
+import Button from "./Button";
+
+interface EmptyStateProps {
+  title?: string;
+  subtitle?: string;
+  showReset?: boolean;
+}
+
+const EmptyState: React.FC<EmptyStateProps> = ({
+  title = "No exact matches found",
+  subtitle = "Try adjusting or removing some of your filters",
+  showReset,
+}) => {
+  const router = useRouter();
+
+  return (
+    <div
+      className="
+        h-[60vh]
+        flex 
+        flex-col 
+        items-center 
+        justify-center
+        "
+    >
+      <Heading center title={title} subtitle={subtitle} />
+      <div className="w-48 mt-4">
+        {showReset && (
+          <Button
+            outline
+            label="Remove all filters"
+            onClick={() => router.push("/")}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default EmptyState;
